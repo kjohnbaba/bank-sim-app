0bd9ab03375a27c5b43fdf01ba6f19524dc0c077
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2024-03-13 21:54:39 -0400

    Created spring mvc exercise project

--- src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
@@ -1,4 +1,23 @@
 package com.cydeo.service.impl;
 
-public class TransactionServiceImpl {
+import com.cydeo.model.Account;
+import com.cydeo.model.Transaction;
+import com.cydeo.service.TransactionService;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public class TransactionServiceImpl implements TransactionService {
+    @Override
+    public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
+
+
+        return null;
+    }
+
+    @Override
+    public List<Transaction> findAllTransaction() {
+        return null;
+    }
 }
